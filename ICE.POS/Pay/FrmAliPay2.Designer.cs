da3495f048224d4be76e19ebff0e0c1b060ee192
xmosnewone <8868814+xmosnewone@user.noreply.gitee.com>
2024-09-13 23:34:40 +0800

    微信和支付宝扫码IMEMod关闭

--- ICE.POS/Pay/FrmAliPay2.Designer.cs
+++ ICE.POS/Pay/FrmAliPay2.Designer.cs
@@ -47,8 +47,9 @@
             this.label1.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.label1.ForeColor = System.Drawing.Color.White;
             this.label1.Location = new System.Drawing.Point(1, -1);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(338, 43);
+            this.label1.Size = new System.Drawing.Size(451, 54);
             this.label1.TabIndex = 0;
             this.label1.Text = "支付宝扫码支付";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -60,25 +61,29 @@
             this.panel1.Controls.Add(this.AliPayAmt);
             this.panel1.Controls.Add(this.label3);
             this.panel1.Controls.Add(this.label2);
-            this.panel1.Location = new System.Drawing.Point(5, 45);
+            this.panel1.Location = new System.Drawing.Point(7, 56);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(321, 102);
+            this.panel1.Size = new System.Drawing.Size(428, 128);
             this.panel1.TabIndex = 1;
             // 
             // mention
             // 
             this.mention.AutoSize = true;
-            this.mention.Location = new System.Drawing.Point(8, 80);
+            this.mention.Location = new System.Drawing.Point(11, 100);
+            this.mention.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.mention.Name = "mention";
-            this.mention.Size = new System.Drawing.Size(0, 12);
+            this.mention.Size = new System.Drawing.Size(0, 15);
             this.mention.TabIndex = 4;
             // 
             // auth_code
             // 
             this.auth_code.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.auth_code.Location = new System.Drawing.Point(9, 44);
+            this.auth_code.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.auth_code.Location = new System.Drawing.Point(12, 55);
+            this.auth_code.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.auth_code.Name = "auth_code";
-            this.auth_code.Size = new System.Drawing.Size(309, 29);
+            this.auth_code.Size = new System.Drawing.Size(411, 35);
             this.auth_code.TabIndex = 3;
             this.auth_code.Tag = "scancodetxt";
             // 
@@ -86,35 +91,39 @@
             // 
             this.AliPayAmt.AutoSize = true;
             this.AliPayAmt.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
-            this.AliPayAmt.Location = new System.Drawing.Point(228, 11);
+            this.AliPayAmt.Location = new System.Drawing.Point(304, 14);
+            this.AliPayAmt.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.AliPayAmt.Name = "AliPayAmt";
-            this.AliPayAmt.Size = new System.Drawing.Size(17, 12);
+            this.AliPayAmt.Size = new System.Drawing.Size(22, 15);
             this.AliPayAmt.TabIndex = 2;
             this.AliPayAmt.Text = "￥";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(184, 12);
+            this.label3.Location = new System.Drawing.Point(245, 15);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(47, 12);
+            this.label3.Size = new System.Drawing.Size(60, 15);
             this.label3.TabIndex = 1;
             this.label3.Text = "应收款:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(7, 12);
+            this.label2.Location = new System.Drawing.Point(9, 15);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(137, 12);
+            this.label2.Size = new System.Drawing.Size(172, 15);
             this.label2.TabIndex = 0;
             this.label2.Text = "请扫描客户支付宝付款码";
             // 
             // surepay
             // 
-            this.surepay.Location = new System.Drawing.Point(5, 154);
+            this.surepay.Location = new System.Drawing.Point(7, 192);
+            this.surepay.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.surepay.Name = "surepay";
-            this.surepay.Size = new System.Drawing.Size(95, 33);
+            this.surepay.Size = new System.Drawing.Size(127, 41);
             this.surepay.TabIndex = 2;
             this.surepay.Text = "确认支付";
             this.surepay.UseVisualStyleBackColor = true;
@@ -122,9 +131,10 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(117, 154);
+            this.button2.Location = new System.Drawing.Point(156, 192);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(95, 33);
+            this.button2.Size = new System.Drawing.Size(127, 41);
             this.button2.TabIndex = 3;
             this.button2.Text = "查询支付状态";
             this.button2.UseVisualStyleBackColor = true;
@@ -132,9 +142,10 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(231, 153);
+            this.button3.Location = new System.Drawing.Point(308, 191);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(95, 33);
+            this.button3.Size = new System.Drawing.Size(127, 41);
             this.button3.TabIndex = 4;
             this.button3.Text = "取消支付(ESC)";
             this.button3.UseVisualStyleBackColor = true;
@@ -142,15 +153,15 @@
             // 
             // FrmAliPay2
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(338, 189);
-            this.ControlBox = false;
+            this.ClientSize = new System.Drawing.Size(451, 236);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.surepay);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.Name = "FrmAliPay2";
             this.Text = "支付宝扫码支付";
             this.panel1.ResumeLayout(false);
--- ICE.POS/Pay/FrmWechatPay.Designer.cs
+++ ICE.POS/Pay/FrmWechatPay.Designer.cs
@@ -47,8 +47,9 @@
             this.label1.Font = new System.Drawing.Font("宋体", 14F);
             this.label1.ForeColor = System.Drawing.Color.White;
             this.label1.Location = new System.Drawing.Point(1, -1);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(338, 43);
+            this.label1.Size = new System.Drawing.Size(451, 54);
             this.label1.TabIndex = 0;
             this.label1.Text = "微信扫码支付";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -60,9 +61,10 @@
             this.panel1.Controls.Add(this.mention);
             this.panel1.Controls.Add(this.auth_code);
             this.panel1.Controls.Add(this.label2);
-            this.panel1.Location = new System.Drawing.Point(5, 46);
+            this.panel1.Location = new System.Drawing.Point(7, 58);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(321, 102);
+            this.panel1.Size = new System.Drawing.Size(428, 128);
             this.panel1.TabIndex = 1;
             // 
             // wxPayAmt
@@ -70,9 +72,10 @@
             this.wxPayAmt.AutoSize = true;
             this.wxPayAmt.Font = new System.Drawing.Font("宋体", 10F, System.Drawing.FontStyle.Bold);
             this.wxPayAmt.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(181)))), ((int)(((byte)(73)))));
-            this.wxPayAmt.Location = new System.Drawing.Point(218, 9);
+            this.wxPayAmt.Location = new System.Drawing.Point(291, 11);
+            this.wxPayAmt.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.wxPayAmt.Name = "wxPayAmt";
-            this.wxPayAmt.Size = new System.Drawing.Size(22, 14);
+            this.wxPayAmt.Size = new System.Drawing.Size(26, 17);
             this.wxPayAmt.TabIndex = 4;
             this.wxPayAmt.Tag = "wxPayAmt";
             this.wxPayAmt.Text = "￥";
@@ -80,29 +83,32 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(175, 10);
+            this.label3.Location = new System.Drawing.Point(233, 12);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(47, 12);
+            this.label3.Size = new System.Drawing.Size(60, 15);
             this.label3.TabIndex = 3;
             this.label3.Text = "应收款:";
             // 
             // mention
             // 
             this.mention.AutoSize = true;
-            this.mention.Location = new System.Drawing.Point(9, 82);
+            this.mention.Location = new System.Drawing.Point(12, 102);
+            this.mention.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.mention.Name = "mention";
-            this.mention.Size = new System.Drawing.Size(0, 12);
+            this.mention.Size = new System.Drawing.Size(0, 15);
             this.mention.TabIndex = 2;
             this.mention.Tag = "mention";
             // 
             // auth_code
             // 
             this.auth_code.Font = new System.Drawing.Font("宋体", 14F);
-            this.auth_code.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.auth_code.Location = new System.Drawing.Point(9, 44);
+            this.auth_code.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.auth_code.Location = new System.Drawing.Point(12, 55);
+            this.auth_code.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.auth_code.MaxLength = 20;
             this.auth_code.Name = "auth_code";
-            this.auth_code.Size = new System.Drawing.Size(309, 29);
+            this.auth_code.Size = new System.Drawing.Size(411, 34);
             this.auth_code.TabIndex = 1;
             this.auth_code.Tag = "scancodetxt";
             this.auth_code.TextChanged += new System.EventHandler(this.onScancode);
@@ -110,17 +116,19 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(7, 10);
+            this.label2.Location = new System.Drawing.Point(9, 12);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(125, 12);
+            this.label2.Size = new System.Drawing.Size(157, 15);
             this.label2.TabIndex = 0;
             this.label2.Text = "请扫描客户微信付款码";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(231, 154);
+            this.button1.Location = new System.Drawing.Point(308, 192);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(95, 33);
+            this.button1.Size = new System.Drawing.Size(127, 41);
             this.button1.TabIndex = 4;
             this.button1.Text = "取消支付(ESC)";
             this.button1.UseVisualStyleBackColor = true;
@@ -129,9 +137,10 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(120, 154);
+            this.button2.Location = new System.Drawing.Point(160, 192);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(95, 33);
+            this.button2.Size = new System.Drawing.Size(127, 41);
             this.button2.TabIndex = 3;
             this.button2.Text = "查询支付状态";
             this.button2.UseVisualStyleBackColor = true;
@@ -139,9 +148,10 @@
             // 
             // surepay
             // 
-            this.surepay.Location = new System.Drawing.Point(5, 155);
+            this.surepay.Location = new System.Drawing.Point(7, 194);
+            this.surepay.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.surepay.Name = "surepay";
-            this.surepay.Size = new System.Drawing.Size(95, 33);
+            this.surepay.Size = new System.Drawing.Size(127, 41);
             this.surepay.TabIndex = 2;
             this.surepay.Text = "确认已支付";
             this.surepay.UseVisualStyleBackColor = true;
@@ -149,14 +159,15 @@
             // 
             // FrmWechatPay
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(338, 189);
+            this.ClientSize = new System.Drawing.Size(451, 236);
             this.Controls.Add(this.surepay);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.Name = "FrmWechatPay";
             this.Text = "微信扫码支付";
             this.panel1.ResumeLayout(false);
