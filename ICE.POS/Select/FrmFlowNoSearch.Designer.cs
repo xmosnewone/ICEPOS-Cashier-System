b94d3fbe76264f904ff67f44467f8bfa06767016
xmosnewone <8868814+xmosnewone@user.noreply.gitee.com>
2024-10-22 22:28:59 +0800

    退货单窗口"原单号"输入控件修改

--- ICE.POS/Select/FrmFlowNoSearch.Designer.cs
+++ ICE.POS/Select/FrmFlowNoSearch.Designer.cs
@@ -28,9 +28,9 @@ namespace ICE.POS
             this.btnOK = new ICE.POS.Common.ButtonEx(this.components);
             this.lbMemo = new System.Windows.Forms.Label();
             this.lbReasonId = new System.Windows.Forms.Label();
-            this.txtInput = new ICE.POS.Common.TextBoxEx(this.components);
             this.panel1 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -38,9 +38,10 @@ namespace ICE.POS
             // 
             this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
             this.btnCancel.ForeColor = System.Drawing.Color.Transparent;
-            this.btnCancel.Location = new System.Drawing.Point(213, 106);
+            this.btnCancel.Location = new System.Drawing.Point(284, 132);
+            this.btnCancel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnCancel.Name = "btnCancel";
-            this.btnCancel.Size = new System.Drawing.Size(94, 34);
+            this.btnCancel.Size = new System.Drawing.Size(125, 42);
             this.btnCancel.TabIndex = 24;
             this.btnCancel.Text = "取消(&Esc)";
             this.btnCancel.UseVisualStyleBackColor = true;
@@ -49,9 +50,10 @@ namespace ICE.POS
             // btnOK
             // 
             this.btnOK.ForeColor = System.Drawing.Color.Transparent;
-            this.btnOK.Location = new System.Drawing.Point(55, 106);
+            this.btnOK.Location = new System.Drawing.Point(73, 132);
+            this.btnOK.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnOK.Name = "btnOK";
-            this.btnOK.Size = new System.Drawing.Size(96, 34);
+            this.btnOK.Size = new System.Drawing.Size(128, 42);
             this.btnOK.TabIndex = 23;
             this.btnOK.Text = "确定(&Enter)";
             this.btnOK.UseVisualStyleBackColor = true;
@@ -61,30 +63,22 @@ namespace ICE.POS
             // 
             this.lbMemo.AutoSize = true;
             this.lbMemo.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.lbMemo.Location = new System.Drawing.Point(17, 66);
+            this.lbMemo.Location = new System.Drawing.Point(23, 82);
+            this.lbMemo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lbMemo.Name = "lbMemo";
-            this.lbMemo.Size = new System.Drawing.Size(56, 14);
+            this.lbMemo.Size = new System.Drawing.Size(71, 18);
             this.lbMemo.TabIndex = 21;
             this.lbMemo.Text = "原单号:";
             // 
             // lbReasonId
             // 
             this.lbReasonId.AutoSize = true;
-            this.lbReasonId.Location = new System.Drawing.Point(25, 117);
+            this.lbReasonId.Location = new System.Drawing.Point(33, 146);
+            this.lbReasonId.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lbReasonId.Name = "lbReasonId";
-            this.lbReasonId.Size = new System.Drawing.Size(0, 12);
+            this.lbReasonId.Size = new System.Drawing.Size(0, 15);
             this.lbReasonId.TabIndex = 26;
             this.lbReasonId.Visible = false;
-            // 
-            // txtInput
-            // 
-            this.txtInput.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.txtInput.Location = new System.Drawing.Point(79, 63);
-            this.txtInput.MaxLength = 40;
-            this.txtInput.Name = "txtInput";
-            this.txtInput.Size = new System.Drawing.Size(265, 23);
-            this.txtInput.TabIndex = 28;
-            this.txtInput.TextType = 1;
             // 
             // panel1
             // 
@@ -92,8 +86,9 @@ namespace ICE.POS
             this.panel1.Controls.Add(this.label1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(367, 45);
+            this.panel1.Size = new System.Drawing.Size(489, 56);
             this.panel1.TabIndex = 28;
             // 
             // label1
@@ -103,25 +98,35 @@ namespace ICE.POS
             this.label1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.label1.ForeColor = System.Drawing.Color.Blue;
             this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(365, 43);
+            this.label1.Size = new System.Drawing.Size(487, 54);
             this.label1.TabIndex = 0;
             this.label1.Text = "原始单号录入";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
+            // txtInput
+            // 
+            this.txtInput.Font = new System.Drawing.Font("宋体", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtInput.Location = new System.Drawing.Point(100, 81);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(308, 28);
+            this.txtInput.TabIndex = 29;
+            // 
             // FrmFlowNoSearch
             // 
             this.AcceptButton = this.btnOK;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.btnCancel;
-            this.ClientSize = new System.Drawing.Size(367, 164);
+            this.ClientSize = new System.Drawing.Size(489, 205);
             this.Controls.Add(this.txtInput);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.lbReasonId);
             this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.btnOK);
             this.Controls.Add(this.lbMemo);
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.Name = "FrmFlowNoSearch";
             this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
@@ -135,8 +140,8 @@ namespace ICE.POS
         private ButtonEx btnOK;
         private System.Windows.Forms.Label lbMemo;
         private System.Windows.Forms.Label lbReasonId;
-        private TextBoxEx txtInput;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtInput;
     }
 }
